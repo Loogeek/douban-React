89b2a7c4e7ef7025d24b6b19f43ec1d430d79054
Loogeek <loogeek@163.com>
2016-03-13 09:43:44 +0800

    修改缩进格式

--- public/scripts/js/movie/movie_admin.js
+++ public/scripts/js/movie/movie_admin.js
@@ -5,7 +5,7 @@ $.support.cors = true;																	// 解决IE8/9 Ajax跨域请求问题
 $(function() {
   // 获取电影列表删除按钮类名，当点击删除按钮触发删除事件
   $('.movieDel').click(function(e) {
-    var target = $(e.target),
+  	var target = $(e.target),
         id = target.data('id'),  // 获取点击的id值
         tr = $('.item-id-' + id);// 获取点击删除按钮所在行
 
